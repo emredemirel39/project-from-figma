b641f4f04a584637499ef973981966b3c74838ea
emredemirel39 <emredemirel39@gmail.com>
2022-06-11 13:51:36 +0300

    Created Hero Bottom section

---
+++ src/components/HeroBottom/index.tsx
@@ -0,0 +1,16 @@
+import styles from './HeroBottom.module.scss';
+
+const HeroBottom = () => {
+  return (
+    <div className={styles.heroBottom}>
+        <button>שומיש יאנת</button>
+        <button>רושיא</button>
+        <p>
+            .ונלש שומיש יאנתלו תויגועב שומישל המכסה הווהמ רתהב השילגה ךשמה .רתויב הבוטה
+             היווחה תא ךרובע רוציל לכונש תנמ לע תויגועב םישמתשמ ונחנא
+        </p>        
+    </div>
+  )
+}
+
+export default HeroBottom;
--- src/views/MainPage/index.tsx
+++ src/views/MainPage/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/Hero'
+import HeroBottom from '../../components/HeroBottom'
 
 const MainPage = () => {
   return (
     <div>
       <Hero/>
+      <HeroBottom/>
     </div>
   )
 }
