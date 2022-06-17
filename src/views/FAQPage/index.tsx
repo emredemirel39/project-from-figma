a4b8022cbd9727022555100c618a8bceceedd0b2
emredemirel39 <emredemirel39@gmail.com>
2022-06-17 21:11:44 +0300

    Created F.A.Q. page

--- src/views/FAQPage/index.tsx
+++ src/views/FAQPage/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+//styles
+import styles from './FAQPage.module.scss';
+
+// components
+import AccordionItem from '../../components/AccordionItem';
+import CalendlyWidget from '../../components/CalendlyWidget';
+import FAQAccordionIems from '../../utils/FAQPageAccordionItems';
+
 const FAQPage = () => {
   return (
-    <div>FAQPage</div>
+    <div>
+      <div className={styles.accordionSection}>
+        <h1>הלאש לכ לע תונעל חמשנ</h1>
+        <div className={styles.accordionWrapper}>
+          {
+            FAQAccordionIems.map(item => <AccordionItem item={item} key={item.id}/>)
+          }
+        </div>
+      </div>
+      <CalendlyWidget/>
+    </div>
   )
 }
 
